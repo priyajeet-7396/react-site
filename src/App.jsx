64841e786a57b363a7c33fc720501b007fc81ba8
unknown <priyajeetsaxena@gmail.com>
2023-04-13 14:46:26 +0530

    navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
@@ -6,10 +7,12 @@ import Gallery from './pages/gallery/Gallery'
 import Plans from './pages/plans/Plans'
 import Trainers from './pages/trainers/Trainers'
 import NotFound from './pages/notFound/NotFound'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+        <Navbar/>
         <Home/>
         <About/>
         <Contact/>
@@ -17,7 +20,7 @@ const App = () => {
         <Plans/>
         <Trainers/>
         <NotFound/>
-    </div>
+    </BrowserRouter>
   )
 }
 
