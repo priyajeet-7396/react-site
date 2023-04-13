73aa86557d184f9cbc30578ff5c5aa1a3c0eff7c
unknown <priyajeetsaxena@gmail.com>
2023-04-13 23:21:46 +0530

    general css styles and variables

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,41 @@
+import { useState } from 'react'
 import {Link,NavLink} from 'react-router-dom'
 import './navbar.css'
 import Logo from '../images/logo.png'
 import {links} from '../data'
 import{GoThreeBars} from 'react-icons/go'
+import {MdOutlineClose} from 'react-icons/md'
  
 
 
 
 const Navbar = () => {
+
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+
   return (
     <nav>
         <div className='container nav__container'>
-            <Link to="/" className='logo'>
+            <Link to="/" className='logo' onClick={() => setIsNavShowing(false)}>
                 <img src={Logo}  alt='navlogo' />
             </Link>
-            <ul className='nav__links'>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav' }`}>
+
                 {
                     links.map(({name,path},index) => {
                         return(
-                            <li>
-                                <NavLink to ={path}>{name}</NavLink>
+                            <li key={index}>
+                                <NavLink to ={path} className={({isActive}) => isActive ? 'active-nav' : ' '} onClick={() =>setIsNavShowing(prev => !prev)}>{name}</NavLink>
                             </li>
                         )
                     })
                 }
             </ul>
-            <button className='nav__toggle-btn'>
-                <GoThreeBars/>
+            <button className='nav__toggle-btn' onClick={() =>setIsNavShowing(prev => !prev)}>
+              {
+                isNavShowing ? <MdOutlineClose/>  : <GoThreeBars/>
+              }
             </button>
         </div>
     </nav>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+
+import Header from '../../components/Header'
 import './home.css'
+
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
